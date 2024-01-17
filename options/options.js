c5f0f2042f512fd0784a0a6f2599bc45dd30f943
Crowdbotics <team@crowdbotics.com>
2024-01-17 11:02:29 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-band-45400",
+  url: "https://billowing-band-45400.botics.co",
+  api: "https://billowing-band-45400.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
